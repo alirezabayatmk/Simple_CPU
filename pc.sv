bf97cb32071facd5575e73aec6fa29ffbe0bb9a7
alirezabayatmk <alireza.slimshady@gmail.com>
2019-12-28 21:42:50 +0330

    pc changed

--- pc.sv
+++ pc.sv
@@ -3,20 +3,36 @@
 
 `include "clk.sv"
 
-module pc(input clk, input write, input clear, input [63:0] i, output [63:0] pc);
 
-reg [63:0] pc;
-clock clock1(clk);
+module PC(clock, reset, PCin, PCout);
 
-always@(posedge clk)
-    begin
-        if(write)
-            begin
-                pc <= 64'd0;
-            end
-        else
-            begin
-                pc <= i;
-            end             
+    input clock, reset;
+    input [31:0] PCin;
+    
+    output reg [31:0] PCout;
+    
+    always @(posedge clock) begin
+        if (reset == 1) 
+            PCout <= 0;
+        else 
+            PCout <= PCin + 4; 
     end
 endmodule
+
+// module pc(input clk, input write, input clear, input [63:0] i, output [63:0] pc);
+
+// reg [63:0] pc;
+// clock clock1(clk);
+
+// always@(posedge clk)
+//     begin
+//         if(write)
+//             begin
+//                 pc <= 64'd0;
+//             end
+//         else
+//             begin
+//                 pc <= i;
+//             end             
+//     end
+// endmodule
