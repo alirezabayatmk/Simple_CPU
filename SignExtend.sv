d327825d56ac58cdc3552e8e205a8074b890883b
alirezabayatmk <alireza.slimshady@gmail.com>
2019-12-28 22:19:32 +0330

    sign extend added

--- SignExtend.sv
+++ SignExtend.sv
@@ -3,7 +3,7 @@ module SignExtend (inst15_0, Extend32);
 	input [15:0] inst15_0;
 	output reg [31:0] Extend32;
 
-	always @(inst15_0) begin
+	always @(posedge clock) begin
 		Extend32[31:0] <= inst15_0[15:0];
 	end
 endmodule
