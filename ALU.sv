960c7231c7a285a63c52acff531f2ade805fbc00
alirezabayatmk <alireza.slimshady@gmail.com>
2019-12-29 09:58:23 +0330

    ALU bit numbers corrected

--- ALU.sv
+++ ALU.sv
@@ -1,9 +1,9 @@
 module ALU (ALUCtl, A, B, ALUOut, Zero);
 
 	input [3:0] ALUCtl;
-	input [31:0] A,B;
+	input [63:0] A,B;
 	
-	output reg [31:0] ALUOut;
+	output reg [63:0] ALUOut;
 	output Zero;
 	assign Zero = (ALUOut == 0);
 	
